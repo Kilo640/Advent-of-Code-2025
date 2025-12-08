fed84fd466d590a82b9a2033b973f1d4213afa52
Kilo640 <131413966+Kilo640@users.noreply.github.com>
2025-12-07 19:03:31 -0600

    Day 4-2 complete
    that was way easier than day 3 lol

---
+++ Day4/SolutionP2.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class SolutionP2{
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        Scanner file = null;
+        boolean fileFound = false;
+        
+        System.out.print("Enter Input File: ");
+        while(!fileFound){
+            try {
+                file = new Scanner(new File(console.nextLine()));
+                fileFound = true;
+            } catch (FileNotFoundException bruh) {
+                System.out.print("Invalid file! Enter Input File: ");
+            }
+        }
+        
+        ArrayList<char[]> map = new ArrayList<>();
+        System.out.println("Map:");
+        while(file.hasNextLine()){
+            map.add(file.nextLine().toCharArray());
+            System.out.println(Arrays.toString(map.getLast()));
+        }
+        System.out.println();
+
+        int rolls = 0;
+        int foundRolls;
+        do{
+            foundRolls = 0;
+            for(int i = 0; i < map.size(); i++){
+                for(int j = 0; j < map.get(i).length; j++){
+                    int adjCount = 0;
+                    if(map.get(i)[j] != '@'){continue;}
+
+                    int[][] adjIndexes = {{i-1,j-1}, {i+1,j-1}, {i-1,j+1}, {i+1,j+1}, {i,j-1}, {i-1,j}, {i,j+1}, {i+1,j}};
+                    for(int k = 0; k < 8; k++){
+                        try {
+                            if(map.get(adjIndexes[k][0])[adjIndexes[k][1]] == '@'){adjCount++;}
+                        } catch(IndexOutOfBoundsException e) {}
+                    }
+                    if(adjCount < 4){
+                        System.out.println("Roll Accessed: (" + i + "," + j + ")");
+                        map.get(i)[j] = '.';
+                        rolls++;
+                        foundRolls++;
+                    }
+                }
+            }
+        }while(foundRolls > 0);
+        System.out.println("Answer: " + rolls);
+    }
+}
